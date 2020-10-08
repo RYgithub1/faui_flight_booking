47b5242f706846fc20d7f3e7c35d8bb158910474
RYgithub1 <ryo.rec.otani@gmail.com>
2020-10-08 23:27:30 +0900

    add tableFlightInfo

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,6 @@ import 'package:flutter/material.dart';
 import 'ticket_list.dart';
 
 
-// Air craft company
-const kSingaporeLogoUrl = 'https://user-images.githubusercontent.com/7200238/82220821-1ebc8880-995a-11ea-9d77-07edda64f05c.png';
-const kQantasLogoUrl = 'https://user-images.githubusercontent.com/7200238/82220824-1fedb580-995a-11ea-8124-f59daff4ebda.png';
-const kEmiratesLogoUrl = 'https://user-images.githubusercontent.com/7200238/82220816-1c5a2e80-995a-11ea-921d-38b3f991d8d2.png';
-const kHainanLogoUrl = 'https://user-images.githubusercontent.com/7200238/82223309-73adce00-995d-11ea-98c0-2dba4e094aca.png';
-
-
 void main(List<String> args) {runApp(MyApp());}
 
 
@@ -30,6 +23,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomPadding: false,
       appBar: AppBar(
         leading: IconButton(
           icon: Icon(Icons.arrow_back_ios),
@@ -47,104 +41,108 @@ class HomeScreen extends StatelessWidget {
         elevation: 0,
         backgroundColor: Colors.indigo[400],
       ),
-      body: Column(
-        children: [
-          Container(
-            padding: EdgeInsets.only(top:5.0, bottom:20.0),
-            color: Colors.indigo[400],
-            child: Row(
-              children: [
-                Expanded(
-                  flex:1,
-                  child: Container(
-                    width: 150.0,
-                    child: Column(
-                      children: [
-                        Text("HND", style:TextStyle(fontSize:45.0, color: Colors.white)),
-                        Text("Tokyo/Haneda", style:TextStyle(fontSize:20.0, color: Colors.grey[400])),
-                      ],
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                padding: EdgeInsets.only(top:5.0, bottom:20.0),
+                color: Colors.indigo[400],
+                child: Row(
+                  children: [
+                    Expanded(
+                      flex:1,
+                      child: Container(
+                        width: 150.0,
+                        child: Column(
+                          children: [
+                            Text("HND", style:TextStyle(fontSize:45.0, color: Colors.white)),
+                            Text("Tokyo/Haneda", style:TextStyle(fontSize:20.0, color: Colors.grey[400])),
+                          ],
+                        ),
+                      ),
                     ),
-                  ),
-                ),
-                Container(
-                  width: 64,
-                  height: 64,
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.white),
-                    borderRadius: BorderRadius.circular(44),
-                  ),
-                  child: Transform.rotate(
-                    angle: pi/2,  /// [pi=180 degrees]
-                    child: Icon(
-                      Icons.flight,
-                      size: 40.0,
-                      color: Colors.white,
+                    Container(
+                      width: 64,
+                      height: 64,
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.white),
+                        borderRadius: BorderRadius.circular(44),
+                      ),
+                      child: Transform.rotate(
+                        angle: pi/2,  /// [pi=180 degrees]
+                        child: Icon(
+                          Icons.flight,
+                          size: 40.0,
+                          color: Colors.white,
+                        ),
+                      ),
                     ),
-                  ),
+                    Expanded(
+                      flex:1,
+                      child: Container(
+                        width: 150.0,
+                        child: Column(
+                          children: [
+                            Text("LDN", style:TextStyle(fontSize:45.0, color: Colors.white)),
+                            Text("London", style:TextStyle(fontSize:20.0, color: Colors.grey[400])),
+                          ],),
+                      ),
+                    ),
+                  ],
                 ),
-                Expanded(
-                  flex:1,
-                  child: Container(
-                    width: 150.0,
-                    child: Column(
-                      children: [
-                        Text("LDN", style:TextStyle(fontSize:45.0, color: Colors.white)),
-                        Text("London", style:TextStyle(fontSize:20.0, color: Colors.grey[400])),
-                      ],),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Container(
-            alignment: Alignment.topLeft,
-            padding: EdgeInsets.only(left: 12.0),
-            width: double.infinity,
-            height: 60.0,
-            child: Text(
-              "Monday, 30 May, 2033",
-              style:TextStyle(fontSize:20.0, color: Colors.grey[400]),
-            ),
-            decoration: BoxDecoration(
-              border: Border.all(color: Colors.indigo[400]),
-              borderRadius: BorderRadius.only(
-                bottomRight: Radius.circular(24),
-                bottomLeft: Radius.circular(24),
               ),
-              color: Colors.indigo[400],
-              boxShadow: [BoxShadow(
-                color: Colors.indigo[200],
-                spreadRadius: 1.0,
-                blurRadius: 10.0,
-                offset: Offset(10, 10),
-              ),],
-            ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.only(left:15.0),
+              Container(
+                alignment: Alignment.topLeft,
+                padding: EdgeInsets.only(left: 12.0),
+                width: double.infinity,
+                height: 60.0,
                 child: Text(
-                  "Tickets",
-                  style: TextStyle(fontSize:20.0),
+                  "Monday, 30 May, 2033",
+                  style:TextStyle(fontSize:20.0, color: Colors.grey[400]),
+                ),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.indigo[400]),
+                  borderRadius: BorderRadius.only(
+                    bottomRight: Radius.circular(24),
+                    bottomLeft: Radius.circular(24),
+                  ),
+                  color: Colors.indigo[400],
+                  boxShadow: [BoxShadow(
+                    color: Colors.indigo[200],
+                    spreadRadius: 1.0,
+                    blurRadius: 10.0,
+                    offset: Offset(10, 10),
+                  ),],
                 ),
               ),
-              Padding(
-                padding: const EdgeInsets.only(right:15.0),
-                child: IconButton(
-                  icon: Icon(Icons.filter_list),
-                  onPressed: (){
-                    Navigator.of(context).push(MaterialPageRoute( builder: (context) {
-                      return NextScreen();
-                    }));
-                  },
-                ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.only(left:15.0),
+                    child: Text(
+                      "Tickets",
+                      style: TextStyle(fontSize:20.0),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(right:15.0),
+                    child: IconButton(
+                      icon: Icon(Icons.filter_list),
+                      onPressed: (){
+                        Navigator.of(context).push(MaterialPageRoute( builder: (context) {
+                          return NextScreen();
+                        }));
+                      },
+                    ),
+                  ),
+                ],
               ),
+              TicketList(),
             ],
           ),
-          TicketList(),
-        ],
+        ),
       ),
     );
   }
--- lib/ticket_list.dart
+++ lib/ticket_list.dart
@@ -1,10 +1,152 @@
 import 'package:flutter/material.dart';
 
+// Air craft company
+const kSingaporeLogoUrl = 'https://user-images.githubusercontent.com/7200238/82220821-1ebc8880-995a-11ea-9d77-07edda64f05c.png';
+const kQantasLogoUrl = 'https://user-images.githubusercontent.com/7200238/82220824-1fedb580-995a-11ea-8124-f59daff4ebda.png';
+const kEmiratesLogoUrl = 'https://user-images.githubusercontent.com/7200238/82220816-1c5a2e80-995a-11ea-921d-38b3f991d8d2.png';
+const kHainanLogoUrl = 'https://user-images.githubusercontent.com/7200238/82223309-73adce00-995d-11ea-98c0-2dba4e094aca.png';
+
+
 class TicketList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        _Ticket(image: Image.network(kSingaporeLogoUrl)),  /// f[TicketList()]t[Ticket()]...[pass namedParameter]
+        _Ticket(image: Image.network(kQantasLogoUrl)),
+        _Ticket(image: Image.network(kEmiratesLogoUrl)),
+        _Ticket(image: Image.network(kHainanLogoUrl)),
+      ],
+    );
+  }
+}
+
+
+/// f[TicketList()]t[Ticket()]
+class _Ticket extends StatelessWidget{
+  final Image image;
+  _Ticket({Key key, @required this.image}):super(key:key);
+
+  @override
+  Widget build(BuildContext context) {
     return Container(
-      
+      width: double.infinity,
+      height: 128,
+      // decoration: ShapeDecoration(
+      //   color: Colors.white,
+      //   // shape: _TicketShapeBorder(width: 1, radius: 16.0),
+      //   shape: null,
+      // ),
+      child: Row(      /// [aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa]
+        children: [
+          Expanded(  /// [ロゴ]
+            flex: 1,
+            child: Container(
+              padding: EdgeInsets.all(24),
+              child: image,
+            ),
+          ),
+          Container(  /// [縦線]
+            width: 1,
+            height: double.infinity,
+            margin: EdgeInsets.symmetric(vertical: 8.0),
+            color: Colors.deepPurple[200],
+          ),
+          Expanded(  /// [table]
+            flex: 2,
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 24),
+              child: Table(
+                children: [
+                  TableRow(
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Departure',
+                            style: TextStyle(color: Colors.deepPurple[300]),
+                          ),
+                          SizedBox(height: 4),
+                          Text(
+                            '04:25 pm',
+                            style: TextStyle(
+                              color: Colors.deepPurple[400],
+                              fontSize: 18,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Arrive',
+                            style: TextStyle(color: Colors.deepPurple[500]),
+                          ),
+                          SizedBox(height: 4),
+                          Text(
+                            '07:55 pm',
+                            style: TextStyle(
+                              color: Colors.deepPurple[600],
+                              fontSize: 18,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  TableRow(
+                    children: [
+                      SizedBox(height: 8),
+                      SizedBox(height: 8),
+                    ],
+                  ),
+                  TableRow(
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Estimation',
+                            style: TextStyle(color: Colors.deepPurple[600]),
+                          ),
+                          SizedBox(height: 4),
+                          Text(
+                            '4h, 30m',
+                            style: TextStyle(
+                              color: Colors.deepPurple[600],
+                              fontSize: 18,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            '\$250.00',
+                            style: TextStyle(
+                              color: Colors.deepPurple[600],
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(height: 4),
+                          Text(
+                            '/person',
+                            style: TextStyle(color: Colors.deepPurple[600]),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
